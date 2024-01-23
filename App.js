1a2af0b531ca0154e149198abeed0a367bfcc309
Ayan <kumarayanatwork@gmail.com>
2024-01-23 23:57:43 +0530

    started with namaste react course

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = <h1>Hello its me Ayan</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
